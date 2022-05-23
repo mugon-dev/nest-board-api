d4b9c2fcc0f72c9367326544a49e86e7aed67028
jmg <jmg41490400@gmail.com>
2022-05-23 15:30:32 +0900

    typeorm 적용, entity 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), BoardsModule],
 })
 export class AppModule {}
