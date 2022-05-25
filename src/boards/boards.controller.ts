c56609244efb7255ad1fe4f117e39d3cf4d978a4
jmg <jmg41490400@gmail.com>
2022-05-25 12:36:36 +0900

    코드 정리

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -20,18 +20,6 @@ import { BoardStatus } from './board-status.enum';
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
-  // @Get()
-  // getAllBoard(): Board[] {
-  //   return this.boardService.getAllBoards();
-  // }
-  //
-  // @Post()
-  // @UsePipes(ValidationPipe)
-  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-  //   return this.boardService.createBoard(createBoardDto);
-  // }
-  //
-
   @Get()
   getAllBoards(): Promise<Board[]> {
     return this.boardService.getAllBoards();
@@ -60,22 +48,4 @@ export class BoardsController {
   ): Promise<Board> {
     return this.boardService.updateBoardStatus(id, status);
   }
-
-  // @Get('/:id')
-  // getBoardById(@Param('id') id: string): Board {
-  //   return this.boardService.getBoardById(id);
-  // }
-  //
-  // @Delete('/:id')
-  // deleteBoard(@Param('id') id: string): void {
-  //   this.boardService.deleteBoard(id);
-  // }
-  //
-  // @Patch('/:id/status')
-  // updateBoardStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  // ): Board {
-  //   return this.boardService.updateBoardStatus(id, status);
-  // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -41,39 +41,4 @@ export class BoardsService {
     await this.boardRepository.save(board);
     return board;
   }
-
-  // getAllBoards(): Board[] {
-  //   return this.boards;
-  // }
-  //
-  // createBoard(createBoardDto: CreateBoardDto) {
-  //   const { title, description } = createBoardDto;
-  //   const board: Board = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: BoardStatus.PUBLIC,
-  //   };
-  //   this.boards.push(board);
-  //   return board;
-  // }
-  //
-  // getBoardById(id: string): Board {
-  //   const found = this.boards.find((board) => board.id === id);
-  //   if (!found) {
-  //     throw new NotFoundException(`Can't find Board with id ${id}`);
-  //   }
-  //   return found;
-  // }
-  //
-  // deleteBoard(id: string): void {
-  //   const found = this.getBoardById(id);
-  //   this.boards = this.boards.filter((board) => board.id !== found.id);
-  // }
-  //
-  // updateBoardStatus(id: string, status: BoardStatus): Board {
-  //   const board = this.getBoardById(id);
-  //   board.status = status;
-  //   return board;
-  // }
 }
