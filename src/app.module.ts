864e30482451184ed0194a1210a053fbf15e3083
jmg <jmg41490400@gmail.com>
2022-05-25 12:58:21 +0900

    auth module setting ,add create user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeormConfig), BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), BoardsModule, AuthModule],
 })
 export class AppModule {}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,12 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
