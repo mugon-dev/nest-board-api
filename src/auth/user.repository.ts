11290e121e3fa838f804fd6eb9ec5c2ff2f8bb4a
jmg <jmg41490400@gmail.com>
2022-05-25 13:54:25 +0900

    password save salt-hashed algorithm, add login

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,11 +6,16 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 
+import * as bcrypt from 'bcryptjs';
+
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
